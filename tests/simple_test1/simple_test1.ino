56bcefe468369a611f57ae71a39d793b70a55fc3
vapirovschi <vapirovschi@synygy.com>
2014-06-03 17:43:46 +0300

    add simple test

---
+++ tests/simple_test1/simple_test1.ino
@@ -0,0 +1,91 @@
+/*
+ Built on Arduino Uno + Wiznet Ethernet shield. 
+ Tested with browser and a POSTMAN chrome plugin
+ 
+ End of line = '13 10' = '\r \n'
+ End of request = empty line = '13 10' = '\r \n'
+ */
+
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+IPAddress ip(10, 220, 10 , 171);
+IPAddress mask(255, 255, 0 , 0);
+IPAddress gateway(10, 220, 0 , 1);
+IPAddress domain(10, 220, 10 , 1);
+
+EthernetServer server(80);
+
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  Ethernet.begin(mac, ip, domain, gateway, mask);
+  server.begin();
+  Serial.print(F("## server is at "));
+  Serial.println(Ethernet.localIP());
+}  
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+
+  if (client) 
+  {
+    Serial.println(F("## new client"));
+
+    // an http request ends with a blank line
+    boolean blank_line = false;
+
+    while (client.connected() && client.available()) 
+    {
+      char c = client.read();      
+
+      Serial.write(c);  
+
+      if (c == '\n' && blank_line) {        
+        // send a standard http response header
+        client.println("HTTP/1.1 200 OK");
+        client.println("Content-Type: text/html");
+        client.println();
+        client.println("<!DOCTYPE HTML>");
+        client.println("<html>");
+        client.print("Hello world from Arduino !!!");                        
+        client.println("</html>");
+        break;
+      }
+
+      if (c == '\n') {
+        blank_line = true;
+      } 
+      else if (c != '\r') {
+        blank_line = false;
+      }      
+    }
+
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println(F("## client disonnected"));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
